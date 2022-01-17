f389c84c4c120d5a4656881ac87e46cfa05e4cdf
SchlechtGut <schlechtgut@mail.ru>
2022-01-17 07:52:00 +0300

    5.1

---
+++ src/main/java/com/example/MyBookShopApp/data/book/file/BookFile.java
@@ -0,0 +1,47 @@
+package com.example.MyBookShopApp.data.book.file;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "book_file")
+public class BookFile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String hash;
+    private String path;
+    private int typeId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public int getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(int typeId) {
+        this.typeId = typeId;
+    }
+}
