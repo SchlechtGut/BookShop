a09453118673b4ca11267b0bf23b4a652d5b80dd
SchlechtGut <schlechtgut@mail.ru>
2022-01-19 09:53:11 +0300

    Liquibase changed

--- src/main/java/com/example/MyBookShopApp/config/CommandLineRunnerImp.java
+++ src/main/java/com/example/MyBookShopApp/config/CommandLineRunnerImp.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.logging.Logger;
+
 @Configuration
 public class CommandLineRunnerImp implements CommandLineRunner {
 
